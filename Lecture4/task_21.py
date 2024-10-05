cfdf32f3c62c9aa6ce049315ca493e5d12ca3e91
shubh <shubhamjain0704@gmail.com>
2024-10-05 19:39:33 +0530

    new root

---
+++ Lecture4/task_21.py
@@ -0,0 +1,10 @@
+import math
+number = float(input("enter the no"))
+sq = number ** 2
+print(sq)
+sqroot = math.sqrt(number)
+print(sqroot)
+cube = number * number * number
+print(cube)
+cuberoot = math.cbrt(number)
+print(cuberoot)
