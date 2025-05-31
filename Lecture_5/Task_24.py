06dab12686544dfbc814c95b43ac772a6b995c1c
shubh <shubhamjain0704@gmail.com>
2025-05-31 18:27:13 +0530

    12FebPractice

--- Lecture_5/Task_24.py
+++ Lecture_5/Task_24.py
@@ -5,6 +5,6 @@ year = int(input("Enter the year:"))
 if (year % 4 ==0 and year % 100 != 0):
     print("Input Value is Leap Year")
 elif(year % 400 ==0):
-    print("Input Value is a leap year")
+    print(f"{year} is a leap year")
 else:
-    print("Input is not a leap year")
+    print(f"{year} is not a leap year")
